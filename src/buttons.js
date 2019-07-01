efdd4c3aed509dc186617bd726163faf83869037
elizhang227 <zhang.eli@hotmail.com>
2019-07-01 11:18:00 -0400

    adding negatives works now

--- src/buttons.js
+++ src/buttons.js
@@ -10,7 +10,6 @@ class Buttons extends Component {
         e.preventDefault();
 
         let num = this.state.test + e.target.innerHTML
-        // num += e.target.innerHTML
         console.log('this is num', num)
         this.setState({
             number: num,
@@ -24,6 +23,66 @@ class Buttons extends Component {
         this.setState({
             number: 0,
             test: ''
+        })
+    }
+
+    compute = async (e) => {
+        e.preventDefault();
+
+        let currentString = this.state.test;
+        let numberStringArray;
+        if (currentString[0] === '-') {
+            //currentString = currentString.split('-').join('')
+            numberStringArray = currentString.split(/\+|\*|\//g);
+        } else {
+            numberStringArray = currentString.split(/\+|\-|\*|\//g);
+        }
+        let operatorsArray;
+        if (currentString[0] === '-') {
+            operatorsArray = currentString.replace(/[0-9]|\-|\./g, "").split("");
+        } else {
+            operatorsArray = currentString.replace(/[0-9]|\./g, "").split("");
+        }
+        console.log('yooo', numberStringArray, operatorsArray)
+
+        let numbersArray = [];
+        numberStringArray.forEach(function(number) {
+            numbersArray.push(Number(number));
+        });
+
+        console.log('number array', numbersArray);
+
+        let multiply = operatorsArray.indexOf("*");
+        while (multiply != -1) {
+            numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply + 1]);
+            operatorsArray.splice(multiply, 1); // removes one instance of the multiply, effectively deletes it from array because no replacement is given
+            multiply = operatorsArray.indexOf('*');
+        }
+    
+        let divide = operatorsArray.indexOf("/");
+        while (divide != -1) {
+            numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide + 1]);
+            operatorsArray.splice(divide, 1);
+            divide = operatorsArray.indexOf('/');
+        }
+    
+        let add = operatorsArray.indexOf("+");
+        while (add != -1) {
+            numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add + 1]);
+            operatorsArray.splice(add, 1);
+            add = operatorsArray.indexOf('+');
+        }
+    
+        let subtract = operatorsArray.indexOf("-");
+        while (subtract != -1) {
+            numbersArray.splice(subtract, 2, numbersArray[subtract] - numbersArray[subtract + 1]);
+            operatorsArray.splice(subtract, 1);
+            subtract = operatorsArray.indexOf('-');
+        }
+
+        this.setState({
+            number: numbersArray,
+            test: numbersArray
         })
     }
 
@@ -61,7 +120,7 @@ class Buttons extends Component {
                             <button onClick={(e) => this.clearScreen(e)}>C</button>
                         </div>
                         <div className='Equal-Button'>
-                            <button>=</button>
+                            <button onClick={(e) => this.compute(e)}>=</button>
                         </div>
                     </div>
 
